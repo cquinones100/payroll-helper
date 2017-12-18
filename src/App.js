93e531f32b12e36bf2dc3174078fced433025f9c
cquinones100 <cquinones100@gmail.com>
2017-12-18 10:19:20 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      employeeData: [],
+      locationData: [],
+      uploadedFile: null
+    }
+  }
+
+  onChange = (event) => {
+    event.preventDefault()
+    this.setState({
+      uploadedFile: event.target.files[0]
+    }, (event) => {
+      const reader = new FileReader()
+      reader.onload = (event) => this.processData(event.target.result)
+      reader.readAsText(this.state.uploadedFile)
+      document.querySelector('#file-upload').value = ''
+    })
+  }
+
+  isEmployeeData = (array) => {
+    return array
+            .filter(line => line.split(',').length === 5)
+              .length === array.length -1 &&
+                /app data/.exec(this.state.uploadedFile.name)
+  }
+
+  processData = (text, title) => {
+    const lines = text.split(/\r\n|\n/)
+    if (this.isEmployeeData(lines)) {
+      this.setState({
+        employeeData: lines
+      }, )
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.onSubmit}>
+          <input id="file-upload" type="file" onChange={this.onChange} accept=".csv"/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
